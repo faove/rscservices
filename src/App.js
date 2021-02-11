48bfd81326512f733e162af209aab00826275669
Francisco Alvarez <faove@hotmail.com>
2021-02-10 22:30:04 -0300

    Ajustando css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import ClientTable from './components/ClientTable'
-import ClientEditForm from './components/ClientEditForm'
-import ClientAddForm from './components/ClientAddForm'
+import Client from './components/ClientTable';
+import Dashboard from './components/DashboardTable';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <ClientTable/>
-    </div>
+    <Router>
+      <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" className="nav-link active">
+                  Dashboard
+                </Link> 
+              </li>
+              <li className="nav-item">
+                <Link to="/client" className="nav-link">
+                  Client
+                </Link> 
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Switch>
+        <Route patch="/client" exact>
+            <Client />    
+        </Route>
+        <Route patch="/" exact>
+            <Dashboard />    
+        </Route>
+      </Switch>  
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/DashboardTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardTable = () => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default DashboardTable;
