4aad40cd98de44699061db5ad346138f0417a3d8
Francisco Alvarez <faove@hotmail.com>
2021-03-03 00:40:00 -0300

    fix autoload

--- src/components/ServicesAddForm.jsx
+++ src/components/ServicesAddForm.jsx
@@ -12,25 +12,20 @@ import { useForm, Controller } from 'react-hook-form';
 const ServicesAddForm = (props) => {
 
     const dispatch = useDispatch()
-    
     const client = useSelector(store => store.client.array)
     const [searchTerm, setSearchTerm] = useState("");
-
     const [searchResults, setSearchResults] = useState([]);
     const [startDate, setStartDate] = useState(new Date());
     const { register, setValue, reset, handleSubmit, errors, control } = useForm({defaultValues: searchResults});
-
-
-
+    const [svalue, setSValue] = useState(null);
+    const [inputValue, setInputValue] = useState('');
 
     const handleChange = event => {
-        console.log('handleChange:')
-        console.log(event.target.value)
-        
+        // console.log('handleChange:')
+        // console.log(event.target.value)
         setSearchTerm(event.target.value);
-        console.log(searchTerm)
+        // console.log(searchTerm)
         // console.log(searchResults)
-        
         // setRefreshKey(oldKey => oldKey + 1)
     };
 
@@ -38,25 +33,28 @@ const ServicesAddForm = (props) => {
 
     const handleClient =  e => {
         e.preventDefault();
-        let regex = /\d*/;
-        let str = e.target.value;
+        // let regex = /\d*/;
+        // let str = e.target.value;
         // let result = regex.test(str);
-        let result = regex.exec(str);
-        str = result[0].trim();
-        console.log(str);
+        // let result = regex.exec(str);
+        // str = result[0].trim();
+        // console.log(str);
         
+        // console.log('handleClient')
+        // console.log(inputValue)
+        // console.log(svalue)
 
-        console.log('handleClient')
-        
-        const array = searchResults.filter(item => item.dni === parseInt(str))
+        setSValue(svalue)
+        // const array = searchResults.filter(item => item.dni === parseInt(str))
         // setSearchResults(results)
-        
-        console.log(array.dni)
+        // console.log(array.dni)
+        // console.log(svalue)
         // console.log(array[0].id,array[0].name, array[0].last_name)
-        // setValue('id', array.id);
-        // setValue('name', array.name);
-        // setValue('last_name', array.last_name);
-        reset(array[0])
+        // setValue('id', svalue.id);
+        // setValue('name', svalue.name);
+        // setValue('last_name', svalue.last_name);
+        reset(svalue)
+        // console.log(value.newValue)
         // console.log('results:::::::::::::::::',results)
         // console.log(searchResults[0].name)
         // console.log(searchResults[0].last_name)
@@ -67,8 +65,7 @@ const ServicesAddForm = (props) => {
     // },[searchResults])
 
     useEffect(() => {
-    
-        // console.log(' useeEffect')
+        console.log(' useeEffect')
         dispatch(getClient());
         // console.log(client)
         const results = client.filter(searchclient =>
@@ -84,19 +81,42 @@ const ServicesAddForm = (props) => {
       setValue('name', data.name);
       setValue('last_name', data.last_name);
       // limpiar campos
-      e.target.reset();
+    //   e.target.reset();
     }
 
     return (
         <Fragment>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div style={{ width: 500 }}>
+
+                {/* <Autocomplete
+                    options={searchResults}
+                    getOptionLabel={(option) => option.name + ' ' + option.last_name}
+                    id="controlled-demo"
+                    value={svalue}
+                    onChange={(event, newValue) => {
+                    setSValue(newValue);
+                    }}
+                    renderInput={(params) => 
+                        <TextField {...params} label="controlled" margin="normal" 
+                        onChange={handleChange} onSelect={handleClient}
+                    />}
+                /> */}
+
                 <Autocomplete
-                  id="search"
-                  disableClearable
-                  options={searchResults.map((option) => option.dni + ' ' + option.name + ' ' + option.last_name)}
-                  // value={searchResults}
-                  filterSelectedOptions
+                    id="search"
+                    disableClearable
+                    options={searchResults}
+                    getOptionLabel={(option) => option.name + ' ' + option.last_name}
+                    value={svalue}
+                    onChange={(event, newValue) => {
+                        setSValue(newValue);
+                        }}
+                    inputValue={inputValue}
+                    onInputChange={(event, newInputValue) => {
+                        setInputValue(newInputValue);
+                    }}
+                    filterSelectedOptions
                 //   options={searchResults.map((option) => option.id + ' ' + option.name + ' ' + option.last_name)}
                   renderInput={(params) => (
                     <TextField
@@ -109,18 +129,12 @@ const ServicesAddForm = (props) => {
                       // onSelect={handleClient(searchResults.name,"name")}
                       // onSelect={handleChange}
                       onSelect={handleClient}
-                      InputProps={{ ...params.InputProps, 
-                        inputRef: params.innerRef,
-                        type: 'search' }}
+                    //   InputProps={{ ...params.InputProps, 
+                    //     inputRef: params.innerRef,
+                    //     type: 'search' }}
                     /> 
                   )}
-                  
                 />
-
-                
-                </div>
-                <div>
-                
                 </div>
                 <div>
                 <input 
